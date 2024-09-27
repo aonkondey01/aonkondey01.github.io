8edc1520440dcd4bc6ea9717adc4cef92d85ecf1
Aonkon Dey <107419903+aonkondey01@users.noreply.github.com>
2024-09-27 01:51:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,27 @@ function nextSlide() {
   currentSlide = (currentSlide < slides.children.length - 1) ? currentSlide + 1 : 0;
   slides.style.transform = `translateX(-${currentSlide * 100}%)`;
 }
+
+const imageFolder = 'images/intro_images';
+const imageList = [
+  'IMG_20230731_124154_814.jpg',
+  'IMG_20230731_125400_151.jpg',
+  'IMG_20230929_142012_752.jpg',
+  'IMG_20230929_143300_768.jpg',
+  'IMG_20230731_124154_982.jpg',
+  'IMG_20230929_142012_112.jpg',
+  'IMG_20230929_142013_076.jpg',
+  'IMG_20240511_182858_671.jpg'
+];
+// Dynamically populate the image slider with images from the list
+function loadImages() {
+  const slider = document.querySelector('.image-slider');
+  imageList.forEach(image => {
+    const imgElement = document.createElement('img');
+    imgElement.src = imageFolder + image;
+    imgElement.alt = `Image ${image}`;
+    slider.appendChild(imgElement);
+  });
+}
+
+window.onload = loadImages;
